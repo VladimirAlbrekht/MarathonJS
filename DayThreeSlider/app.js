545610c82f4946a56b17387883d2a154d502d770
Vladimir Albrekht <vladimir_albreht@mail.ru>
2023-09-03 23:27:48 +0800

    Marathon: day 3: alternative slider

---
+++ DayThreeSlider/app.js
@@ -0,0 +1,39 @@
+const upButton = document.querySelector('.up-button')
+const downButton = document.querySelector('.down-button')
+const sidebar = document.querySelector('.sidebar')
+
+
+const mainSlide = document.querySelector('.main-slide')
+const slidesCount = mainSlide.querySelectorAll('div').length
+const container = document.querySelector('.container')
+
+
+let activeSlideIndex = 0
+
+sidebar.style.top = `-${(slidesCount - 1) * 100}vh`;
+
+upButton.addEventListener('click', ()=> {changeSlide('up')})
+downButton.addEventListener('click',()=> {changeSlide('down')})
+
+function changeSlide(direction) {
+    if (direction === 'up') {
+        console.log('click')
+        activeSlideIndex++
+        if (activeSlideIndex === slidesCount) {
+            activeSlideIndex = 0
+        }
+    } else if (direction === 'down') {
+        activeSlideIndex--
+        if (activeSlideIndex < 0) {
+            activeSlideIndex = slidesCount - 1
+        }
+    }
+
+
+    const height = container.clientHeight 
+
+    mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`
+
+
+    sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`
+}
